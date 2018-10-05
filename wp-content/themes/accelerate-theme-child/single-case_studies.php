b3ede342db8e43ccf3587e9017b6a1ab41ef63aa
unknown <leighgdamico@gmail.com>
2018-10-05 17:22:13 -0500

    End of 203.9

---
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template for displaying single case Studies
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content sidebar">
+		<div class="main-content" role="main">
+			<?php while ( have_posts() ) : the_post();
+          /*Define variables to use custom fields */
+          $services = get_field('services');
+          $client = get_field('client');
+          $link = get_field('site_link');
+          $image_1 = get_field('image_1');
+          $image_2 = get_field('image_2');
+				  $image_3 = get_field('image_3');
+          $size = "full";?>
+
+        <article class="case-study">
+          <aside class="case-study-sidebar">
+            <h2><?php the_title(); ?></h2>
+            <h4><?php echo $services; ?></h4>
+            <h3>Client: <?php echo $client; ?></h3>
+
+            <?php the_content(); ?>
+
+            <p class="read-more-link"><a href="<?php echo $link ?>">Visit Live Site</a></p>
+          </aside>
+          <aside class="case-study-images">
+            <?php if($image_1) {
+              echo wp_get_attachment_image($image_1, $size);
+            } ?>
+            <?php if($image_2) {
+              echo wp_get_attachment_image($image_2, $size);
+            } ?>
+            <?php if($image_3) {
+              echo wp_get_attachment_image($image_3, $size);
+            } ?>
+          </aside>
+        </article>
+
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- .main-content -->
+
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
